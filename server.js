b53287166bfdfeae658500414bfdef2e6722a085
vmanamino <vmanamino@gmail.com>
2016-07-27 03:04:05 +0000

    parallified

--- server.js
+++ server.js
@@ -17,8 +17,24 @@ var getFromApi = function(endpoint, args) {
     return emitter;
 };
 
+var getTopTracks = function(artist, callback){
+    
+    unirest.get('https://api.spotify.com/v1/artists/'+artist.id+'/top-tracks?country=US')
+        .end(function(response){
+            if (response.ok){
+              artist.tracks = response.body.tracks;
+              callback();
+            }
+            else {
+              callback(response.error);
+            }
+        });
+};
+
 var app = express();
 app.use(express.static('public'));
+
+
 
 app.get('/search/:name', function(req, res) {
     var searchReq = getFromApi('search', {
@@ -36,14 +52,31 @@ app.get('/search/:name', function(req, res) {
                 if(response.ok){
                     // console.log(response.body.artists)
                     artist.related = response.body.artists;
-                    console.log(artist)
+                    var total = artist.related.length;
+                    var completed = 0;
+                    // res.json(artist);
+                    
+                    var checkCompleted = function(){
+                        if (completed == total){
+                            res.json(artist);
+                        }
+                    }
+                    for (var i = 0; i < artist.related.length; i++){
+                        getTopTracks(artist.related[i], function(err){
+                            if(err){
+                                res.sendStatus(404)
+                            }
+                            completed += 1;
+                            checkCompleted();
+                        })
+                    }
                 }
                 else {
                     res.sendStatus(404);
                 }
             })
         
-        res.json(artist);
+        
     });
 
     searchReq.on('error', function(code) {
